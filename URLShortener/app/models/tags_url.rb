510a49fa56bc2ed9026627720fe2e6060ba704f3
Vincent <vhchang@gmail.com>
2013-01-24 22:28:08 -0800

    Update URLShortener/app/models/tags_url.rb

--- URLShortener/app/models/tags_url.rb
+++ URLShortener/app/models/tags_url.rb
@@ -1,5 +1,5 @@
-class TagsUrl < ActiveRecord::Base
-  # attr_accessible :tag_id, :url_id
+class TagsUrl < ActiveRecord::Base      #Vincent - maybe change the file name to be "tagsurl.rb" to be consistent with
+  # attr_accessible :tag_id, :url_id    # model name.
   belongs_to :tag
   belongs_to :url
 end
